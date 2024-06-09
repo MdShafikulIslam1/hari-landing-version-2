5050ee37279ff6b4fddde931b011260b122259af
shofikul <179398ssshofik@gmail.com>
2024-06-09 23:21:08 +0600

    fix: clock

--- src/components/common/Clock.tsx
+++ src/components/common/Clock.tsx
@@ -1,15 +1,58 @@
+// export default function Clock({ variant }: { variant?: "one" | "two" }) {
+//     const color = variant === "two" ? "bg-[rgb(36,16,12)]": "bg-orange-500";
+
+//     return (
+//       <div className="flex items-center gap-2">
+//         <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>00</div>
+//         <span className="font-bold text-2xl">:</span>
+
+//         <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>23</div>
+//         <span className="font-bold text-2xl">:</span>
+
+//         <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>30</div>
+//       </div>
+//     )
+//   }
+
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Clock({ variant }: { variant?: "one" | "two" }) {
-    const color = variant === "two" ? "bg-[rgb(36,16,12)]": "bg-orange-500";
+  const [time, setTime] = useState(new Date());
 
-    return (
-      <div className="flex items-center gap-2">
-        <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>00</div>
-        <span className="font-bold text-2xl">:</span>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
 
-        <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>23</div>
-        <span className="font-bold text-2xl">:</span>
-        
-        <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>30</div>
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTimeUnit = (unit: number) => String(unit).padStart(2, "0");
+
+  const color = variant === "two" ? "bg-[rgb(36,16,12)]" : "bg-orange-500";
+
+  return (
+    <div className="flex items-center gap-2">
+      <div
+        className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}
+      >
+        {formatTimeUnit(time.getHours())}
       </div>
-    )
-  }
+      <span className="font-bold text-2xl">:</span>
+
+      <div
+        className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}
+      >
+        {formatTimeUnit(time.getMinutes())}
+      </div>
+      <span className="font-bold text-2xl">:</span>
+
+      <div
+        className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}
+      >
+        {formatTimeUnit(time.getSeconds())}
+      </div>
+    </div>
+  );
+}
