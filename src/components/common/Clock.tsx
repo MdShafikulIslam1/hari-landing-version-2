ea68d0643c78cd52cfd02f60073501b772b9e638
shofikul <179398ssshofik@gmail.com>
2024-06-10 20:33:54 +0600

    fix bg image

--- src/components/common/Clock.tsx
+++ src/components/common/Clock.tsx
@@ -1,19 +1,3 @@
-// export default function Clock({ variant }: { variant?: "one" | "two" }) {
-//     const color = variant === "two" ? "bg-[rgb(36,16,12)]": "bg-orange-500";
-
-//     return (
-//       <div className="flex items-center gap-2">
-//         <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>00</div>
-//         <span className="font-bold text-2xl">:</span>
-
-//         <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>23</div>
-//         <span className="font-bold text-2xl">:</span>
-
-//         <div className={`w-14 h-14 grid place-items-center text-2xl font-bold ${color} rounded-lg shadow-md`}>30</div>
-//       </div>
-//     )
-//   }
-
 "use client";
 import { useEffect, useState } from "react";
 
@@ -21,11 +5,19 @@ export default function Clock({ variant }: { variant?: "one" | "two" }) {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
+    const intervalId = setInterval(() => {
+      const now = new Date();
+
+      // Check if a new day has started
+      if (now.getDate() !== time.getDate()) {
+        setTime(now);
+      } else {
+        // Update time normally if it's still the same day
+        setTime(now);
+      }
     }, 1000);
 
-    return () => clearInterval(interval);
+    return () => clearInterval(intervalId);
   }, []);
 
   const formatTimeUnit = (unit: number) => String(unit).padStart(2, "0");
--- src/components/home/Faq.tsx
+++ src/components/home/Faq.tsx
@@ -1,20 +1,39 @@
+import Image from "next/image";
+import recommendedImg from "../../../public/recomended_img.jpg";
 import { FAQS } from "../../constances";
 import { CheckIcon } from "../common";
 
 export default function Faq() {
-    return (
-      <section className="">
-        <div className="md:px-8 md:py-10 holder bg-no-repeat bg-cover bg-right md:bg-right-top text-black rounded-xl" style={{ backgroundImage: "url('/recomended_img.jpg')" }}>
-          <div className="md:w-4/6 px-8 py-10 md:p-0 flex flex-col gap-4 bg-gradient-to-b md:bg-none from-white/50 to-black/50 rounded-xl">
-            {FAQS.map(item => (
-              <div className="flex items-start gap-2" key={item}>
-                <CheckIcon />
-                <p className="font-bold">{item}</p>
-              </div>
-            ))}
-          </div>
-          <div className="md:w-2/6"></div>
+  return (
+    <section className="">
+      <div className="md:px-8 block md:hidden md:py-10 holder bg-no-repeat bg-cover bg-right md:bg-right-top text-black rounded-xl">
+        <div className="block md:hidden">
+          <Image src={recommendedImg} alt="hair image" />
         </div>
-      </section>
-    )
-  }
+        <div className="md:w-4/6 px-8 py-10 md:p-0 flex flex-col gap-4 bg-gradient-to-b md:bg-none from-white/50 to-black/50 rounded-xl">
+          {FAQS.map((item) => (
+            <div className="flex items-start gap-2" key={item}>
+              <CheckIcon />
+              <p className="font-bold">{item}</p>
+            </div>
+          ))}
+        </div>
+        <div className="md:w-2/6"></div>
+      </div>
+      <div
+        className="md:px-8 md:py-10 hidden md:block holder bg-no-repeat bg-cover bg-right md:bg-right-top text-black rounded-xl"
+        style={{ backgroundImage: "url('/recomended_img.jpg')" }}
+      >
+        <div className="md:w-4/6 px-8 py-10 md:p-0 flex flex-col gap-4 bg-gradient-to-b md:bg-none from-white/50 to-black/50 rounded-xl">
+          {FAQS.map((item) => (
+            <div className="flex items-start gap-2" key={item}>
+              <CheckIcon />
+              <p className="font-bold">{item}</p>
+            </div>
+          ))}
+        </div>
+        <div className="md:w-2/6"></div>
+      </div>
+    </section>
+  );
+}
